9958fb09d11f86668f6c8ccd8f7221e8e4a9ff6d
Erik Voit <erikvoit@gmail.com>
2017-07-31 15:48:06 -0500

    Implemented StandardDeviation to extend GeneralStatistics

--- algo/src/SDScalp.java
+++ algo/src/SDScalp.java
@@ -1,6 +1,6 @@
 import com.optionscity.freeway.api.*;
+import common.sdscalp.StandardDeviation;
 import org.jquantlib.math.statistics.GeneralStatistics;
-import org.jquantlib.math.matrixutilities.Array;
 
 /**
  * Created by evoit on 7/31/2017.
@@ -8,6 +8,8 @@ import org.jquantlib.math.matrixutilities.Array;
 public class SDScalp extends AbstractJob {
     // variables
     final double[] fiveMinPriceArrayTestES = {2469.50, 2468.25, 2467.50, 2465.25, 2467.25, 2465.25, 2463.75, 2461.25, 2460.00, 2462.25, 2464.25, 2465.50, 2467.25, 2468.25, 2469.50, 2472.50, 2474.50, 2474.75, 2476.25, 2478.75};
+    GeneralStatistics fiveMinPrices;
+    StandardDeviation fiveMinuteStdDiv;
 
     @Override
     public void install(IJobSetup setup) {
@@ -18,9 +20,8 @@ public class SDScalp extends AbstractJob {
     public void begin(IContainer container) {
         super.begin(container);
 
-        GeneralStatistics fiveMinPrices = null;
-        fiveMinPrices.addSequence(fiveMinPriceArrayTestES);
-
-        log("The Standard deviation of the set is " + fiveMinPrices.standardDeviation());
+        fiveMinuteStdDiv = new StandardDeviation();
+        fiveMinuteStdDiv.addSequence(fiveMinPriceArrayTestES);
+        log("The Standard deviation calculated by my extended class is " + fiveMinuteStdDiv.standardDeviation());
     }
 }
---
+++ common/src/common/sdscalp/StandardDeviation.java
@@ -0,0 +1,9 @@
+package common.sdscalp;
+
+import org.jquantlib.math.statistics.GeneralStatistics;
+
+/**
+ * Created by evoit on 7/31/2017.
+ */
+public class StandardDeviation extends GeneralStatistics {
+}
