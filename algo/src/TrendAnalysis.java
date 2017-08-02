77838acffa11c6383c60313cf40b4d0b12d7333c
Erik Voit <erikvoit@gmail.com>
2017-08-02 13:22:20 -0500

    Added opencsv 3.10 and ta4j 0.8 librarys to project

---
+++ algo/src/TrendAnalysis.java
@@ -0,0 +1,17 @@
+import com.optionscity.freeway.api.AbstractJob;
+import com.optionscity.freeway.api.IContainer;
+import com.optionscity.freeway.api.IJobSetup;
+
+/**
+ * Created by evoit on 8/2/2017.
+ */
+public class TrendAnalysis extends AbstractJob {
+    @Override
+    public void install(IJobSetup iJobSetup) {
+
+    }
+    @Override
+    public void begin(IContainer container) {
+
+    }
+}
