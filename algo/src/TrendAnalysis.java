5e78b23e8fe4399f9a139e377d2b276a7cc08ac5
Erik Voit <erikvoit@gmail.com>
2017-08-02 16:16:28 -0500

    ta4j working in TrendAnalysis.java so cool!!!!!!!!!! Now let's do something with it
    TODO implement cross job signaling. Order logic...

--- algo/src/TrendAnalysis.java
+++ algo/src/TrendAnalysis.java
@@ -1,20 +1,27 @@
-import com.opencsv.CSVReader;
 import com.optionscity.freeway.api.AbstractJob;
 import com.optionscity.freeway.api.IContainer;
 import com.optionscity.freeway.api.IJobSetup;
+import eu.verdelhan.ta4j.Tick;
+import eu.verdelhan.ta4j.TimeSeries;
+import eu.verdelhan.ta4j.indicators.simple.ClosePriceIndicator;
+import eu.verdelhan.ta4j.indicators.trackers.SMAIndicator;
 import org.joda.time.DateTime;
+import com.opencsv.CSVReader;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 /**
  * Created by evoit on 8/2/2017.
  *
- * parse file that was in Date, Open, High, Low, Last, Change, Settle, Volume, Previous Day OI format
+ * parse daily bar file from quandl.com
+ * file was in Date, Open, High, Low, Last, Change, Settle, Volume, Previous Day OI format
+ * NEED TO CHANGE parse order in loadDayBars() if file format changes!!!!!!!
+ *
  */
 public class TrendAnalysis extends AbstractJob {
     CSVReader reader;
@@ -32,11 +39,21 @@ public class TrendAnalysis extends AbstractJob {
     public void begin(IContainer container) {
         super.begin(container);
         dailyBars = getStringVar("DailyBars");
-        //private static final DateFormat DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        loadDayBars();
+        TimeSeries esDailyBars = loadDayBars();
+        log("Time Series " + esDailyBars.getName() + " loaded with " + esDailyBars.getTickCount() + " bars");
+
+        ClosePriceIndicator closePrice = new ClosePriceIndicator(esDailyBars);
+        // Getting the simple moving average (SMA) of the close price over the last 5 ticks
+        SMAIndicator shortSma = new SMAIndicator(closePrice, 5);
+        log("The 5 day SMA is " + shortSma.getValue(esDailyBars.getTickCount()-1));
+        // Getting the Simple moving average (SMA) over the last 200 ticks
+        SMAIndicator twoHundredDaySma = new SMAIndicator(closePrice, 200);
+        log("The 200 day SMA is " + twoHundredDaySma.getValue(esDailyBars.getTickCount()-1));
     }
 
-    public void loadDayBars() {
+    public TimeSeries loadDayBars() {
+        List<Tick> ticks = new ArrayList<>();
+
         try{
             reader = new CSVReader(new FileReader(FILEPATH+dailyBars));
         } catch (FileNotFoundException e) {
@@ -46,6 +63,7 @@ public class TrendAnalysis extends AbstractJob {
             dayBars = reader.readAll();
             dayBars.remove(0); // remove header
             Collections.reverse(dayBars);
+            log("Loading daily bars from " + FILEPATH+dailyBars);
         }catch (IOException e){
             log("IO Exception "+ e);
         }
@@ -57,6 +75,9 @@ public class TrendAnalysis extends AbstractJob {
             String lowPrice = bar[3];
             String closePrice = bar[6];
             String volume = bar[7];
+
+            // add tick to ticks Array list for time series creation
+            ticks.add(new Tick(barDate, openPrice, highPrice, lowPrice,closePrice, volume ));
 
             StringBuilder sb = new StringBuilder();
             sb.append(bar[0]);
@@ -73,6 +94,8 @@ public class TrendAnalysis extends AbstractJob {
             sb.append(",");
             log(sb.toString());
         }
+        // return a time series from daily bars imported from CSV :)
+        return new TimeSeries("ES Continuous Day Bars", ticks);
     }
 
 }
